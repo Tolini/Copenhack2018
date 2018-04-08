d8fa837e1cbcb794d124d56860d036f5e3a8a6ea
Tolini <Toli.TobiasLindstrom@gmail.com>
2018-04-08 12:00:07 +0200

    Cube now respawns if it falls of a platform

---
+++ Assets/Respawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = new Vector3(0, 0.5f, 0);
+            other.transform.rotation = Quaternion.identity;
+        }
+    }
+}
